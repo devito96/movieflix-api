6eef8ebff63c6a2099140000c056064436559ed5
Vitor Johann <johannvitorhugoojeda@gmail.com>
2024-11-20 13:09:05 -0300

    adicionada a remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -70,7 +70,25 @@ app.put("/movies/:id", async (req, res) => {
             data: data
         })
     } catch (error) {
-        return res.status(500).send({ message: "Falha ao atualizar o registro do filme" });
+        return res.status(500).send({ message: `Falha ao atualizar o registro do filme. Código do erro: ${error}` });
+    }
+
+    res.status(200).send();
+})
+
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({ where: { id } });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado!" })
+        }
+
+        await prisma.movie.delete({ where: { id } });
+    } catch (error) {
+        return res.status(500).send({ message: `Falha ao atualizar o registro do filme. Código do erro: ${error}` });
     }
 
     res.status(200).send();
